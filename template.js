acd59cb7ee899b265ac3f60effa297deb4753937
r3dDoX <patrick@walther-online.ch>
2019-03-28 07:41:44 +0100

    setup basic image slider

--- template.js
+++ template.js
@@ -1,12 +1,38 @@
 const html = `
     <style>
+        * {
+            box-sizing: border-box;
+        }
         
+        .slider {
+            display: flex;
+        }
+        
+        img {
+            max-width: 100%;
+            display: none;
+        }
+        
+        .active {
+            display: block;
+            animation: fadeImg 0.8s;
+        }
+        
+        @keyframes fadeImg {
+            from {
+                opacity: 0;
+            }
+            
+            to {
+                opacity: 1;
+            }
+        }
     </style>
-    <div>
-        <img src="img/img1.jpg">
-        <img src="img/img2.jpg">
-        <img src="img/img3.jpg">
-        <img src="img/img4.jpg">
+    <div class="slider">
+      <img src="img/img1.jpg">
+      <img class="active" src="img/img2.jpg">
+      <img src="img/img3.jpg">
+      <img src="img/img4.jpg">
     </div>
 `;
 
--- webcomponent.js
+++ webcomponent.js
@@ -6,6 +6,48 @@ class ImageSlider extends HTMLElement {
 
     let clonedTemplate = getTemplate().content.cloneNode(true);
     this.attachShadow({ mode: 'open' }).appendChild(clonedTemplate);
+
+    document.body.addEventListener('keydown', event => this.onKeyPress(event));
+  }
+
+  onKeyPress(event) {
+      if (event.keyCode == '37') {
+        this.showPreviousItem();
+      } else if (event.keyCode == '39') {
+        this.showNextItem();
+      }
+  }
+
+  showPreviousItem() {
+    const images = Array.from(this.shadowRoot.querySelectorAll('.slider img'));
+    let activeIndex = images.findIndex(image => image.classList.contains('active'));
+    if (activeIndex === 0) {
+      activeIndex = images.length - 1;
+    } else {
+      activeIndex = activeIndex - 1;
+    }
+    this.updateActiveElement(images, activeIndex);
+  }
+
+  showNextItem() {
+    const images = Array.from(this.shadowRoot.querySelectorAll('.slider img'));
+    let activeIndex = images.findIndex(image => image.classList.contains('active'));
+    if (activeIndex === images.length - 1) {
+      activeIndex = 0;
+    } else {
+      activeIndex = activeIndex + 1;
+    }
+    this.updateActiveElement(images, activeIndex);
+  }
+
+  updateActiveElement(images, activeIndex) {
+    images.forEach((image, index) => {
+      if (index === activeIndex) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
   }
 }
 
